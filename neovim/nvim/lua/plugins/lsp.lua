e21d38889d718498cc22adf300eaffbd6ccf8632
Taira_Hoshino <Taira.Hoshino@wdbcoco.com>
2026-02-18 13:59:13 +0900

    feat: Neovim補完LSP設定を追加
    nvim-cmpによる自動補完(LSP/バッファ/パス/LuaSnip)、
    Mason経由のLSPサーバー管理(lua_ls, pyright, gopls,
    rust_analyzer, ts_ls)、LSPキーマップを設定

---
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "gopls", "rust_analyzer", "ts_ls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(_, bufnr)
+        local map = function(keys, func, desc)
+          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        end
+        map("gd", vim.lsp.buf.definition, "Go to definition")
+        map("gr", vim.lsp.buf.references, "Go to references")
+        map("K", vim.lsp.buf.hover, "Hover documentation")
+        map("<leader>rn", vim.lsp.buf.rename, "Rename")
+        map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+      end
+
+      local servers = { "lua_ls", "pyright", "gopls", "rust_analyzer", "ts_ls" }
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+      end
+    end,
+  },
+}
